8480295f4855421cb0df0dc27f3b5374a30a2f91
hangeulisbest <wj100213@nate.com>
2022-01-23 20:21:11 +0900

    login dto 수정

--- backend/src/main/java/com/memo/backend/dto/login/LoginReqDTO.java
+++ backend/src/main/java/com/memo/backend/dto/login/LoginReqDTO.java
@@ -5,7 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-
+/**
+ * LoginReqDTO 설명 : 로그인시 DTO 이메일,패스워드
+ * @author jowonjun
+ * @version 1.0.0
+ * 작성일 : 2022/01/23
+**/
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

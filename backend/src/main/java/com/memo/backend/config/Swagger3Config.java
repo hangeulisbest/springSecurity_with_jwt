0ff647667703a3b22c38e9c1c93a39f0b53cd1ec
hangeulisbest <wj100213@nate.com>
2022-01-25 01:18:56 +0900

    스웨거 설정 변경

--- backend/src/main/java/com/memo/backend/config/Swagger3Config.java
+++ backend/src/main/java/com/memo/backend/config/Swagger3Config.java
@@ -6,8 +6,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+
+import java.util.List;
 
 /**
  * Swagger3Config 설명 : swagger3 API 문서를 만들때 설정
@@ -21,6 +28,8 @@ public class Swagger3Config {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.OAS_30)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .useDefaultResponseMessages(true)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.memo")) // API 문서를 만들어줄 범위
@@ -37,5 +46,22 @@ public class Swagger3Config {
                 .license("No license")
                 .licenseUrl("license url not found")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT","Authorization","header");
     }
 }

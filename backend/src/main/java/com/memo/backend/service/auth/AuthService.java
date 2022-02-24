53fc7968693a605df284d27f6285f8d7161f4d14
hangeulisbest <wj100213@nate.com>
2022-02-24 22:48:55 +0900

    test

--- backend/src/main/java/com/memo/backend/service/auth/AuthService.java
+++ backend/src/main/java/com/memo/backend/service/auth/AuthService.java
@@ -24,8 +24,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
 import java.util.HashSet;
 
 @Slf4j

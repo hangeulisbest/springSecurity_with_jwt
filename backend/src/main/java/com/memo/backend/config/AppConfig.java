8eb278d833a3902ca339e2c602ec4aa4f3d32d9b
hangeulisbest <wj100213@nate.com>
2022-02-27 22:28:26 +0900

    jwtException 추가

--- backend/src/main/java/com/memo/backend/config/AppConfig.java
+++ backend/src/main/java/com/memo/backend/config/AppConfig.java
@@ -11,9 +11,8 @@ public class AppConfig {
      *
      * 2022-02-26
      *
-     * @return PasswordEncoder - Spring5 부터 encoder에 {id} 값을 붙이고 가져와야 비교가 가능하다.
-     * 예를 들어, 어떤 Member의 비밀번호가 1234이고 이를 인코딩하여 "ABS@DS" 로 저장했을때
-     *   passwordEncoder.matches("1234", {noop}ABS@DS )) 로 해야 비교가 가능하다.
+     * @return PasswordEncoder - Spring5 부터 인코더 생성 방법이 변경되었다.
+     *  다양한 암호화 알고리즘을 변경가능하도록 한것으로 보인다.
      *   처음에는 SecurityConfig 에 빈으로 생성하였으나 SecurityConfig 에서 CustomEmailPasswordAuthProvider 를 의존하고
      *   CustomEmailPasswordAuthProvider 는 passwordEncoder(SecurityConfig)에 의존하여 순환참조가 일어나
      *   따로 빈을 만드는 AppConfig를 생성하였다.
@@ -21,6 +20,6 @@ public class AppConfig {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // noop 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // Default 사용
     }
 }
